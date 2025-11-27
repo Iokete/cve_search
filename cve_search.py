eff0849773750650ec1b428f35a3d34c28b80bf3
Iokete <85845820+Iokete@users.noreply.github.com>
2025-11-27 18:23:25 +0100

    cogiendo forma

--- cve_search.py
+++ cve_search.py
@@ -3,6 +3,7 @@ import argparse
 import configparser
 import requests
 import re
+from pprint import pprint
 
 
 cpe_regex = r'''cpe:2\.3:[aho\*\-](:(((\?*|\*?)([a-zA-Z0-9\-\._]|(\\[\\\*\?!"#$$%&'\(\)\+,/:;<=>@\[\]\^`\{\|}~]))+(\?*|\*?))|[\*\-])){5}(:(([a-zA-Z]{2,3}(-([a-zA-Z]{2}|[0-9]{3}))?)|[\*\-]))(:(((\?*|\*?)([a-zA-Z0-9\-\._]|(\\[\\\*\?!"#$$%&'\(\)\+,/:;<=>@\[\]\^`\{\|}~]))+(\?*|\*?))|[\*\-])){4}'''
@@ -15,12 +16,9 @@ class api_conn:
 		try:
 			req = requests.get(self.baseurl, params=param)
 			return req.json()
-		except:
-			print(f"[!] Error\n");
+		except Exception as e:
+			print(f"[!] Error: {e}\n");
 			return None
-
-class CPE:
-	def __init__(self, )
 
 def retrieve_urls():
 	config = configparser.ConfigParser()
@@ -34,24 +32,27 @@ def retrieve_urls():
 
 	return cve_url, cpe_url
 
-def CPEExtractor(query):
+def is_cpe(query): return query.startswith("cpe:2.3:") and len(query.split(":")) > 5
 
-
+def validate_date(date): return re.fullmatch(r'^\d{2}-\d{4}/\d{2}-\d{4}$', date)
 
 def define_parser():
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument("query",help="producto/vendedor/cpe")
-	parser.add_argument("--filter", dest="severity", choices=['CRITICAL', 'HIGH', 'MEDIUM', 'LOW', 'NONE'], help="filtrar por CVSS3 severity")
-	parser.add_argument("--date", dest="date", help="especificar rango de fecha (mm-aaaa/mm-aaaa)", required=False)
+	parser.add_argument("--filter", dest="severity", choices=['CRITICAL', 'HIGH', 'MEDIUM', 'LOW', 'NONE'], help="filtrar por CVSS3 severity - solo cve")
+	parser.add_argument("--date", dest="date", help="especificar rango de fecha (mm-aaaa/mm-aaaa) - solo cve", required=False)
 	parser.add_argument("-f", dest="outfile", help="guardar resultados a un archivo", metavar='FILE', required=False)
 	parser.add_argument("-out", help="imprimir output", required=False, action='store_true', default=True)
 
 	return parser
 
+# Devolver cpe de un nombre random
+def cpe_from_vendor(client, string): return client.make_request({"keywordSearch" : string})
 
-def search_cpe(): return
-def search_vendor(): return
+
+# Devolver cves a partir de un cpe
+def cve_from_cpe(cve_client, args): return
 
 if __name__ == '__main__':
 	p = define_parser()
@@ -62,17 +63,21 @@ if __name__ == '__main__':
 	cve_client = api_conn(cve_url)
 	cpe_client = api_conn(cpe_url)
 
-	query = args.query
-	severity = args.severity
-	date = args.date
-	outfile = args.outfile
-	out = args.out
-
 	# cpe:2.3:a:cisco:ios:*
 
-	if True:
-		print(f"CPE!")
+	if is_cpe(args.query):
+		print(f"[*] Searching CVEs associated to: {args.query}")
+		output = cve_from_cpe(cpe_client, args)
+		if(output['totalResults'] > 0): 
+			pprint(output)
+		else:
+			print(f"[*] No results from query {args.query}")
 	else:
-		print(f"Not a cpe!")
+		print(f"[*] Searching CPEs associated to: {args.query}")
+		output = cpe_from_vendor(cpe_client, args.query)
+		if(output['totalResults'] > 0): 
+			pprint(output)
+		else:
+			print(f"[*] No results from query {args.query}")
 
-	print(args)
+
